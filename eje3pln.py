b9693fa75780543c12575fb1fc04a9fb2fec4280
danielrpg <daniel.fernandezgp@gmail.com>
2026-03-11 20:27:43 -0400

    feat: Add spaCy script for Spanish entity recognition and part-of-speech tagging.

---
+++ eje3pln.py
@@ -0,0 +1,15 @@
+import spacy 
+
+# cargar el modelo en español 
+pln = spacy.load("es_core_news_sm")
+
+# Separacion de palabras
+palabras = pln("Presidente Paz propone bloque de seis naciones para potenciar el desarrollo sudamericano en el 20 de Abril - 2026-04-01")
+
+# cada palabra se puede tokenizar
+for palabra in palabras.ents:
+    print(palabra.text, palabra.label_)
+
+for palabra in palabras.ents:
+    print(palabra.text, palabra.pos_)
+
